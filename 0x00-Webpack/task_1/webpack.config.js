4c19afc21aaebfa4a66ab51a8fe080a7c2a03e72
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-07-22 16:27:29 +0100

    Update webpack.config.js

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: './js/dashboard_main.js',
+  entry: {
+    main: path.resolve(__dirname, './js/dashboard_main.js'),
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public')
