326be2b1b06790bdfbe4e0566644f38e6a6ea606
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-07-22 16:48:45 +0100

    Create dashboard_main.js

---
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,23 @@
+import $ from 'jquery';
+import _ from 'lodash';
+import '../css/main.css';
+
+function updateCounter() {
+  let count = 0;
+  return function() {
+    count += 1;
+    $('#count').text(`${count} clicks on the button`);
+  };
+}
+
+$(document).ready(function() {
+  $('body').append('<div id="logo"></div>');
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button>Click here to get started</button>');
+  $('body').append('<p id="count"></p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+
+  const countUpdate = _.debounce(updateCounter(), 300);
+  $('button').on('click', countUpdate);
+});
