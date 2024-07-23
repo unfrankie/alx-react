7258463519f08e42374139dc8bfde352f8eddc24
unfrankie <anas.iramol@gmail.com>
2024-07-23 16:33:50 +0100

    move files around

---
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+  },
+  devServer: {
+    static: path.resolve(__dirname, '../dist'),
+    hot: true,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$|jsx/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './dist/index.html',
+    }),
+  ],
+  devtool: 'inline-source-map',
+  mode: 'development',
+};
