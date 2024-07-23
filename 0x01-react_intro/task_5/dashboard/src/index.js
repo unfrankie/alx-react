350385394dc5d063e90709058afa78994568c192
unfrankie <anas.iramol@gmail.com>
2024-07-23 16:38:07 +0100

    fixing webpack task

--- 0x01-react_intro/task_5/dashboard/src/index.js
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -1,3 +1,18 @@
-import './style.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import Notifications from "./Notifications";
 
-console.log('Hello, Webpack!');
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+const rootNotifications = ReactDOM.createRoot(document.getElementById("root-notifications"));
+rootNotifications.render(
+  <React.StrictMode>
+    <Notifications />
+  </React.StrictMode>
+);
