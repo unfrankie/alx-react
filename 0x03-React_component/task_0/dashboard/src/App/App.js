ae3855be34622cb8c7e1cdc88e246ee60b28f72a
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-07-31 17:01:55 +0100

    Update App.js

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -8,31 +8,37 @@ import './App.css';
 import PropTypes from 'prop-types';
 import { getLatestNotification } from '../utils/utils';
 
-const listCourses = [
-	{ id: 1, name: 'ES6', credit: 60 },
-	{ id: 2, name: 'Webpack', credit: 20 },
-	{ id: 3, name: 'React', credit: 40 },
-];
+class App extends React.Component {
+	listCourses = [
+		{ id: 1, name: 'ES6', credit: 60 },
+		{ id: 2, name: 'Webpack', credit: 20 },
+		{ id: 3, name: 'React', credit: 40 },
+	];
 
-const listNotifications = [
-	{ id: 1, type: 'default', value: 'New course available' },
-	{ id: 2, type: 'urgent', value: 'New resume available' },
-	{ id: 3, type: 'urgent', html: getLatestNotification() },
-];
+	listNotifications = [
+		{ id: 1, type: 'default', value: 'New course available' },
+		{ id: 2, type: 'urgent', value: 'New resume available' },
+		{ id: 3, type: 'urgent', html: getLatestNotification() },
+	];
 
-function App({ isLoggedIn }) {
-	return (
-		<React.Fragment>
-			<div className='App'>
-				<div className='heading-section'>
-					<Notifications listNotifications={listNotifications} />
-					<Header />
+	render() {
+		return (
+			<React.Fragment>
+				<div className='App'>
+					<div className='heading-section'>
+						<Notifications listNotifications={this.listNotifications} />
+						<Header />
+					</div>
+					{this.props.isLoggedIn ? (
+						<CourseList listCourses={this.listCourses} />
+					) : (
+						<Login />
+					)}
+					<Footer />
 				</div>
-				{isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
-				<Footer />
-			</div>
-		</React.Fragment>
-	);
+			</React.Fragment>
+		);
+	}
 }
 
 App.defaultProps = {
