47b086602dba67145e015594d3ef4c759002b402
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-08-01 11:54:21 +0100

    Update Notifications.js

--- 0x03-React_component/task_5/dashboard/src/Notifications/Notifications.js
+++ 0x03-React_component/task_5/dashboard/src/Notifications/Notifications.js
@@ -11,6 +11,10 @@ class Notifications extends React.Component {
 		this.markAsRead = this.markAsRead.bind(this);
 	}
 
+	shouldComponentUpdate(nextProps) {
+		return nextProps.length > this.props.listNotifications.length;
+	}
+
 	markAsRead(id) {
 		console.log(`Notification ${id} has been marked as read`);
 	}
