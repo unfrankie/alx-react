a81bf7ded72aeef332b0b374c5c1faceab4ce5bf
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-08-01 11:35:47 +0100

    Update NotificationItem.js

--- 0x03-React_component/task_2/dashboard/src/Notifications/NotificationItem.js
+++ 0x03-React_component/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import './Notifications.css';
+import PropTypes from 'prop-types';
 
-const NotificationItem = ({ type, html, value }) => {
-	return (
-		<>
-			{type && value ? <li data-notification-type={type}>{value}</li> : null}
-			{html ? (
-				<li data-urgent dangerouslySetInnerHTML={{ __html: html }}></li>
-			) : null}
-		</>
-	);
+class NotificationItem extends React.Component {
+	render() {
+		const { type, value, html, markAsRead, id } = this.props;
+		return (
+			<React.Fragment>
+				{type && value ? (
+					<li onClick={() => markAsRead(id)} data-notification-type={type}>
+						{value}
+					</li>
+				) : null}
+				{html ? (
+					<li
+						onClick={() => markAsRead(id)}
+						data-urgent
+						dangerouslySetInnerHTML={{ __html: html }}
+					></li>
+				) : null}
+			</React.Fragment>
+		);
+	}
+}
+
+NotificationItem.propTypes = {
+	type: PropTypes.string.isRequired,
+	value: PropTypes.string,
+	__html: PropTypes.shape({
+		html: PropTypes.string,
+	}),
 };
+
+NotificationItem.defaultProps = {
+	type: 'default',
+};
+
 export default NotificationItem;
