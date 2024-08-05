e4c9f8b03c40c58ccfd48cf86facd64df7c8c0f2
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-08-05 16:28:45 +0100

    Update CourseListRow.js

--- 0x04-React_inline_styling/task_0/dashbord/src/CourseList/CourseListRow.js
+++ 0x04-React_inline_styling/task_0/dashbord/src/CourseList/CourseListRow.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const headerStyle = {
+	backgroundColor: '#deb5b545',
+};
+const normalRowStyle = {
+	backgroundColor: '#f5f5f5ab',
+};
 const CourseListRow = ({ isHeader, textFirstCell, textSecondCell }) => {
 	return (
-		<tr>
+		<tr style={normalRowStyle}>
 			{isHeader ? (
 				textSecondCell === null ? (
-					<th colSpan={2}>{textFirstCell}</th>
+					<th style={headerStyle} colSpan={2}>
+						{textFirstCell}
+					</th>
 				) : (
 					<>
-						<th>{textFirstCell}</th>
-						<th>{textSecondCell}</th>
+						<th style={headerStyle}>{textFirstCell}</th>
+						<th style={headerStyle}>{textSecondCell}</th>
 					</>
 				)
 			) : (
