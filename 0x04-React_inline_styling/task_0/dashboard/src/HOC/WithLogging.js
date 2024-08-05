588e718388fdae1daf81f1a06111185c92e16d76
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-08-05 16:58:44 +0100

    Update WithLogging.js

--- 0x04-React_inline_styling/task_0/dashboard/src/HOC/WithLogging.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/HOC/WithLogging.js
@@ -1,26 +1,25 @@
 import React, { Component } from 'react';
 
-const WithLogging = ({ WrappedComponent }) => {
-	const getDisplayName = (WrappedComponent) =>
-		WrappedComponent.displayName || WrappedComponent.name || 'Component';
+const WithLogging = (WrappedComponent) => {
+  class WithLoggingComponent extends Component {
+    componentDidMount() {
+      const name = WrappedComponent.displayName || WrappedComponent.name || 'Component';
+      console.log(`Component ${name} is mounted`);
+    }
 
-	WithLogging.displayName = `WithLogging(${getDisplayName(WrappedComponent)})`;
+    componentWillUnmount() {
+      const name = WrappedComponent.displayName || WrappedComponent.name || 'Component';
+      console.log(`Component ${name} is going to unmount`);
+    }
 
-	return class extends Component {
-		componentDidMount() {
-			console.log(`Component ${getDisplayName(WrappedComponent)} is mounted`);
-		}
+    render() {
+      return <WrappedComponent {...this.props} />;
+    }
+  }
 
-		componentWillUnmount() {
-			console.log(
-				`Component ${getDisplayName(WrappedComponent)} is going to unmount`
-			);
-		}
+  WithLoggingComponent.displayName = `WithLogging(${WrappedComponent.displayName || WrappedComponent.name || 'Component'})`;
 
-		render() {
-			return <WrappedComponent {...this.props} />;
-		}
-	};
+  return WithLoggingComponent;
 };
 
 export default WithLogging;
