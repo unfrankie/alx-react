2bbaf52c076d3397ef4aa83b5017a7736673010b
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-08-05 16:59:48 +0100

    Update BodySectionWithMarginBottom.test.js

--- 0x04-React_inline_styling/task_0/dashboard/src/BodySection/BodySectionWithMarginBottom.test.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/BodySection/BodySectionWithMarginBottom.test.js
@@ -4,12 +4,13 @@ import BodySectionWithMarginBottom from './BodySectionWithMarginBottom';
 import BodySection from './BodySection';
 
 describe('BodySectionWithMarginBottom tests', () => {
-	it('should apply margin bottom to child component', () => {
-		const wrapper = shallow(<BodySectionWithMarginBottom title='test title' />);
-
-		expect(wrapper.find(BodySection)).toHaveLength(1);
-		expect(wrapper.find(BodySection).html()).toEqual(
-			'<div class="bodySection"><h2>test title</h2></div>'
-		);
-	});
+  it('should apply margin bottom to child component', () => {
+    const wrapper = shallow(
+      <BodySectionWithMarginBottom title="test title">
+        <div>Child component</div>
+      </BodySectionWithMarginBottom>
+    );
+    expect(wrapper.find(BodySection)).toHaveLength(1);
+    expect(wrapper.find(BodySection).html()).toContain('Child component');
+  });
 });
