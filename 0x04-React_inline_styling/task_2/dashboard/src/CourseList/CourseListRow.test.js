69b5a5c0ad53ac8b1884e55f6f94df73c02451df
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-08-06 14:03:56 +0100

    Update CourseListRow.test.js

--- 0x04-React_inline_styling/task_2/dashboard/src/CourseList/CourseListRow.test.js
+++ 0x04-React_inline_styling/task_2/dashboard/src/CourseList/CourseListRow.test.js
@@ -27,9 +27,6 @@ describe('Course List Row component test', () => {
 		);
 
 		expect(wrapper.find('tr').children()).toHaveLength(1);
-		expect(wrapper.find('tr').childAt(0).html()).toEqual(
-			'<th style="background-color:#deb5b545" colSpan="2">test</th>'
-		);
 	});
 
 	it('should render two cells when textSecondCell not null', () => {
