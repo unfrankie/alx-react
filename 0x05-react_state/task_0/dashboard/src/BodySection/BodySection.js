2633b9b84c67bc81756e8118f7467b3b1b415203
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-08-10 13:24:11 +0100

    Update BodySection.js

--- 0x05-react_state/task_0/dashboard/src/BodySection/BodySection.js
+++ 0x05-react_state/task_0/dashboard/src/BodySection/BodySection.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
 
-const BodySection = ({ title, children }) => {
-	return (
-		<div className='bodySection'>
-			<h2>{title}</h2>
-			{children}
-		</div>
-	);
-};
+const styles = StyleSheet.create({
+  small: {
+    '@media (max-width: 900px)': {
+      display: 'grid',
+      justifyContent: 'center',
+      width: '50%',
+      textAlign: 'center',
+      marginLeft: 'auto',
+      marginRight: 'auto'
+    }
+  }
+})
+
+export default class BodySection extends Component {
+  render() {
+    const { title, children } = this.props;
+    return (
+      <div className={css(styles.small)}>
+        <h2>{title}</h2>
+        {children}
+      </div>
+    );
+  }
+}
 
 BodySection.propTypes = {
-	title: PropTypes.string,
-	children: PropTypes.oneOfType([
-		PropTypes.arrayOf(PropTypes.node),
-		PropTypes.node,
-	]).isRequired,
-};
-
-export default BodySection;
+  title: PropTypes.string,
+}
