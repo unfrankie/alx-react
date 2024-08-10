d646d182f7456bc5dccc07842358083a0b697cae
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-08-10 13:30:33 +0100

    Update Header.test.js

--- 0x05-react_state/task_0/dashboard/src/Header/Header.test.js
+++ 0x05-react_state/task_0/dashboard/src/Header/Header.test.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import Header from './Header';
-import { shallow } from 'enzyme';
-import { StyleSheetTestUtils } from 'aphrodite';
+import { StyleSheet, css } from 'aphrodite';
+import holberton_logo from '../assets/holberton_logo.jpeg';
 
-beforeEach(() => {
-	StyleSheetTestUtils.suppressStyleInjection();
-});
-afterEach(() => {
-	StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
-});
+const Header = () => {
+  return (
+    <div className={css(styles.header)}>
+      <img src={holberton_logo} className={css(styles.logo)} alt="logo" />
+      <h1 className={css(styles.title)}>School dashboard</h1>
+    </div>
+  );
+}
 
-describe('Header', () => {
-	it('render without crashing', () => {
-		const wrapper = shallow(<Header />);
-		expect(wrapper.exists()).toEqual(true);
-	});
-	it('should render a h1', () => {
-		const wrapper = shallow(<Header />);
-		expect(wrapper.exists('img')).toEqual(true);
-		expect(wrapper.containsMatchingElement(<h1>School dashboard</h1>)).toEqual(
-			true
-		);
-	});
-});
+const styles = StyleSheet.create({
+  header: {
+    display: 'flex',
+    alignItems: 'center',
+    fontSize: 'calc(10px + 2vmin)',
+    color: 'white',
+    '@media (max-width: 900px)': {
+      justifyContent: 'center'
+    }
+  },
+  title: {
+    color: 'red'
+  },
+  logo: {
+    height: '100px',
+    width: '100px'
+  }
+})
+
+export default Header;
