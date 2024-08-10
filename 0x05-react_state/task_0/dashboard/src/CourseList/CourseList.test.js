ae59f4df7979b735deb7be9725ec9b11b53317b6
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-08-10 13:26:47 +0100

    Update CourseList.test.js

--- 0x05-react_state/task_0/dashboard/src/CourseList/CourseList.test.js
+++ 0x05-react_state/task_0/dashboard/src/CourseList/CourseList.test.js
@@ -1,51 +1,33 @@
 import React from 'react';
-import CourseList from './CourseList';
-import CourseListRow from './CourseListRow';
-import { shallow } from 'enzyme';
 import { StyleSheetTestUtils } from 'aphrodite';
+import CourseList from './CourseList';
+import { shallow } from 'enzyme';
 
 beforeEach(() => {
-	StyleSheetTestUtils.suppressStyleInjection();
+  StyleSheetTestUtils.suppressStyleInjection();
 });
+
 afterEach(() => {
-	StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+  StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
 });
 
-const listCourses = [
-	{ id: 1, name: 'ES6', credit: 60 },
-	{ id: 2, name: 'Webpack', credit: 20 },
-	{ id: 3, name: 'React', credit: 40 },
-];
+describe('rendering CourseList component', () => {
+  it('renders Courselist component without crashing', () => {
+    const wrapper = shallow(<CourseList />);
 
-describe('CourseList component tests', () => {
-	it('should render without crashing', () => {
-		const wrapper = shallow(<CourseList />);
+    expect(wrapper.exists()).toBe(true);
+  });
 
-		expect(wrapper.exists()).toBe(true);
-	});
+  it('renders 5 different rows', () => {
+    const listCourses = [
+      { id: 1, name: 'ES6', credit: 60 },
+      { id: 2, name: 'Webpack', credit: 20 },
+      { id: 3, name: 'React', credit: 40 }
+    ];
 
-	it('renders 5 different rows', () => {
-		const wrapper = shallow(<CourseList listCourses={listCourses} />);
+    const wrapper = shallow(<CourseList listCourses={listCourses} />);
 
-		expect(wrapper.find('thead').children()).toHaveLength(2);
-		wrapper.find('thead').forEach((node) => {
-			expect(
-				node.equals(
-					<CourseListRow
-						textFirstCell='Course name'
-						textSecondCell='Credit'
-						isHeader={true}
-					/>
-				)
-			);
-		});
-
-		expect(wrapper.find('tbody').children()).toHaveLength(3);
-	});
-
-	it('renders correctely when passed a list of courses', () => {
-		const wrapper = shallow(<CourseList listCourses={listCourses} />);
-
-		expect(wrapper.find('tbody').children()).toHaveLength(3);
-	});
-});
+    expect(wrapper.find('thead').children()).toHaveLength(2);
+    expect(wrapper.find('tbody').children()).toHaveLength(3);
+  });
+})
