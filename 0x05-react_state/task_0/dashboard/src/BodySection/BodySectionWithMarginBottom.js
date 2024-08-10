de0ad48a504d0a2d1fe026bafa8d5d021f47a8cf
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-08-10 13:25:17 +0100

    Update BodySectionWithMarginBottom.js

--- 0x05-react_state/task_0/dashboard/src/BodySection/BodySectionWithMarginBottom.js
+++ 0x05-react_state/task_0/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -1,28 +1,21 @@
-import React from 'react';
-import BodySection from './BodySection';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import { StyleSheet, css } from 'aphrodite';
+import BodySection from './BodySection';
 
 const styles = StyleSheet.create({
-	bodySectionWithMargin: {
-		marginBottom: '40px',
-	},
-});
+  bodySectionWithMargin: {
+    marginBottom: '40px'
+  }
+})
 
-const BodySectionWithMarginBottom = ({ title, children }) => {
-	return (
-		<div className={css(styles.bodySectionWithMargin)}>
-			<BodySection title={title}>{children}</BodySection>
-		</div>
-	);
-};
-
-BodySectionWithMarginBottom.propTypes = {
-	title: PropTypes.string,
-	children: PropTypes.oneOfType([
-		PropTypes.arrayOf(PropTypes.node),
-		PropTypes.node,
-	]),
-};
-
-export default BodySectionWithMarginBottom;
+export default class BodySectionWithMarginBottom extends Component {
+  render() {
+    const { children } = this.props
+    return (
+      <div className={css(styles.bodySectionWithMargin)}>
+        <BodySection />
+        {children}
+      </div>
+    );
+  }
+}
