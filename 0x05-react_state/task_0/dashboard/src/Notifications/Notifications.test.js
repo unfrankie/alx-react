8a6b173b92376a561dd2dc5509d982cfaa6ad5bf
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-08-11 00:15:21 +0100

    Update Notifications.test.js

--- 0x05-react_state/task_0/dashboard/src/Notifications/Notifications.test.js
+++ 0x05-react_state/task_0/dashboard/src/Notifications/Notifications.test.js
@@ -3,13 +3,6 @@ import { shallow } from 'enzyme';
 import Notifications from './Notifications';
 
 describe('<Notifications />', () => {
-  it('should call handleDisplayDrawer when clicking on menu item', () => {
-    const handleDisplayDrawer = jest.fn();
-    const wrapper = shallow(<Notifications handleDisplayDrawer={handleDisplayDrawer} />);
-    wrapper.find('.menuItem').simulate('click');
-    expect(handleDisplayDrawer).toHaveBeenCalled();
-  });
-
   it('should call handleHideDrawer when clicking on close button', () => {
     const handleHideDrawer = jest.fn();
     const wrapper = shallow(<Notifications displayDrawer={true} handleHideDrawer={handleHideDrawer} />);
