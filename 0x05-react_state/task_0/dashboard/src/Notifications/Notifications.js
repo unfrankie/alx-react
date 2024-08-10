c3e7b41d3430e88fd725b1711150ec0defb4abd6
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-08-10 13:07:14 +0100

    Update Notifications.js

--- 0x05-react_state/task_0/dashboard/src/Notifications/Notifications.js
+++ 0x05-react_state/task_0/dashboard/src/Notifications/Notifications.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { StyleSheet, css } from 'aphrodite';
-import closeIcon from './close-icon.png';
+import closeIcon from "../assets/close-icon.png";
 import NotificationItem from './NotificationItem';
 import NotificationItemShape from './NotificationItemShape';
 import PropTypes from 'prop-types';
