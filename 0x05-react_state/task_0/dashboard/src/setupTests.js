4788dd4510dbd2d553bd5d316cd4e81d7737c256
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-08-11 00:41:26 +0100

    Update setupTests.js

--- 0x05-react_state/task_0/dashboard/src/setupTests.js
+++ 0x05-react_state/task_0/dashboard/src/setupTests.js
@@ -1,5 +1,6 @@
 import { configure } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { JSDOM } from 'jsdom';
 
 configure({ adapter: new Adapter() });
 
@@ -7,18 +8,15 @@ const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
 const { window } = jsdom;
 
 function copyProps(src, target) {
-  const props = Object.getOwnPropertyNames(src)
-    .filter(prop => typeof target[prop] === 'undefined')
-    .reduce((result, prop) => ({
-      ...result,
-      [prop]: Object.getOwnPropertyDescriptor(src, prop),
-    }), {});
-  Object.defineProperties(target, props);
+  Object.defineProperties(target, {
+    ...Object.getOwnPropertyDescriptors(src),
+    ...Object.getOwnPropertyDescriptors(target)
+  });
 }
 
 global.window = window;
 global.document = window.document;
 global.navigator = {
-  userAgent: 'node.js',
+  userAgent: 'node.js'
 };
 copyProps(window, global);
