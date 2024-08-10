47d6bd8427ff7103493ead2c808240b14835b45d
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-08-11 00:44:23 +0100

    Update setupTests.js

--- 0x05-react_state/task_0/dashboard/src/setupTests.js
+++ 0x05-react_state/task_0/dashboard/src/setupTests.js
@@ -1,10 +1,4 @@
 import { configure } from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-import { JSDOM } from 'jsdom';
 
 configure({ adapter: new Adapter() });
-
-if (typeof global.TextEncoder === 'undefined') {
-  global.TextEncoder = require('util').TextEncoder;
-  global.TextDecoder = require('util').TextDecoder;
-}
