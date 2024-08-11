6b292a749dc326c5b2ff14a89ac15f8814ee0db8
unfrankie <anas.iramol@gmail.com>
2024-08-11 12:58:26 +0100

    all tasks

--- 0x05-react_state/task_0/dashboard/src/Header/Header.js
+++ 0x05-react_state/task_0/dashboard/src/Header/Header.js
@@ -1,11 +1,11 @@
 import React from "react";
-import holberton_logo from "../assets/holberton_logo.jpg";
+import holbertonLogo from "../assets/holberton_logo.jpg";
 import { StyleSheet, css } from "aphrodite";
 
 function Header() {
   return (
     <div className={css(styles.header)}>
-      <img src={holberton_logo} className={css(styles.headerImg)} />
+      <img src={holbertonLogo} className={css(styles.headerImg)} />
       <h1>School dashboard</h1>
     </div>
   );
